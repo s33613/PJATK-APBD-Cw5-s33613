e15b8130dfcfd7a4add54c2d28de4be7731b425b
s33613 <267266068+s33613@users.noreply.github.com>
2026-04-19 14:58:03 +0200

    Added Room classes and a simple GetAllRooms HttpGet method

---
+++ PJATK-APBD-Cw5-s33613/Controllers/RoomsController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using PJATK_APBD_Cw5_s33613.DTO;
+using PJATK_APBD_Cw5_s33613.Objects;
+
+namespace PJATK_APBD_Cw5_s33613.Controllers;
+[ApiController]
+[Route("api/[controller]")]
+
+public class RoomsController : ControllerBase
+{
+    public static List<Room> rooms = [
+    new Room()
+    {
+        id = 1,
+        name = "Room VIP",
+        buildingCode = "a1",
+        floor = 3,
+        capacity = 5,
+        hasProjector = true,
+        isActive = true
+    },
+    new Room(){
+        id = 2,
+        name = "Room 101",
+        buildingCode = "a1",
+        floor = 1,
+        capacity = 5,
+        hasProjector = false,
+        isActive = true
+    },
+    new Room()
+    {
+        id = 3,
+        name = "Room 102",
+        buildingCode = "a1",
+        floor = 1,
+        capacity = 5,
+        hasProjector = false,
+        isActive = true
+    }];
+    [HttpGet]
+    public IActionResult GetAllRooms()
+    {
+        return Ok(rooms.Select(e => new RoomDTO()
+        {
+            id = e.id,
+            name = e.name,
+            buildingCode = e.buildingCode,
+            floor = e.floor,
+            capacity = e.capacity,
+            hasProjector = e.hasProjector,
+            isActive = e.isActive
+        }));
+    }
+}
---
+++ PJATK-APBD-Cw5-s33613/DTO/RoomDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace PJATK_APBD_Cw5_s33613.DTO;
+
+public class RoomDTO
+{
+    public int id { get; set; }
+    public string name { get; set; }
+    public string buildingCode { get; set; }
+    public int floor { get; set; }
+    public int capacity { get; set; }
+    public bool hasProjector  { get; set; }
+    public bool isActive { get; set; }
+}
